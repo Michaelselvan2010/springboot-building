c896db78db9b1591129a3c116713f56c7a43d350
Michael <michael.selvan2005@gmail.com>
2021-07-25 18:08:52 +0530

    Second Commit.

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,7 @@ package com.stacksimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// This is first Program
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
