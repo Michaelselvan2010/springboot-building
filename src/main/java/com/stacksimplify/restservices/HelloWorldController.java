04e3f656b659bda5d649690ccb14b3cb937bd3d3
Michael <michael.selvan2005@gmail.com>
2021-07-25 19:26:02 +0530

    HelloWorld Base

---
+++ src/main/java/com/stacksimplify/restservices/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.stacksimplify.restservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller
+@RestController 
+public class HelloWorldController {
+
+	
+	//Simple Method
+	// URI -/helloworld
+	//GET 
+	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@GetMapping("/helloworld2")
+	public String helloWorld() {
+		return "Hello World2";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		
+		return new UserDetails("Kalyan","Reddy","Hyderabad");
+	}
+}
