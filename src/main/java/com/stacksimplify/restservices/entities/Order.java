d77a9c42a56e7a240b35dc9df9c20921ac051ae1
Michael <michael.selvan2005@gmail.com>
2021-07-27 19:19:01 +0530

    JPA-OneToMany-FirstCommit

---
+++ src/main/java/com/stacksimplify/restservices/entities/Order.java
@@ -0,0 +1,65 @@
+package com.stacksimplify.restservices.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="orders")
+public class Order {
+
+	@Id
+	//@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private Long orderid;
+	private String orderdescription;
+	
+	@ManyToOne(fetch =FetchType.LAZY)
+	@JsonIgnore
+	private UserInfo userInfo;
+
+	public Order() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getOrderid() {
+		return orderid;
+	}
+
+	public void setOrderid(Long orderid) {
+		this.orderid = orderid;
+	}
+
+	public String getOrderdescription() {
+		return orderdescription;
+	}
+
+	public void setOrderdescription(String orderdescription) {
+		this.orderdescription = orderdescription;
+	}
+
+	public UserInfo getUserInfo() {
+		return userInfo;
+	}
+
+	public void setUserInfo(UserInfo userInfo) {
+		this.userInfo = userInfo;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
