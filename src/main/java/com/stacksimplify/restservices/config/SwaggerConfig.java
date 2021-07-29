3f2a0e38f56e1bbc95c1fac398df5381b839b398
Michael <michael.selvan2005@gmail.com>
2021-07-30 04:04:54 +0530

    Swagger Second Commit

--- src/main/java/com/stacksimplify/restservices/config/SwaggerConfig.java
+++ src/main/java/com/stacksimplify/restservices/config/SwaggerConfig.java
@@ -3,8 +3,12 @@ package com.stacksimplify.restservices.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.stacksimplify.restservices.services.Contact;
+
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,6 +20,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(getApiInfo())
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
@@ -25,7 +30,15 @@ public class SwaggerConfig {
 	//Swagger  Metadata: http://localhost:8080/v2/api-docs
 	//Swagger UI URL: http://localhost:8080/swagger-ui.html
 	
-	
+	private ApiInfo getApiInfo() {
+		return new ApiInfoBuilder()
+				.title("Stacksimplyfied User Management Service")
+				.description("This page lists all API of user Management")
+				.version("2.0")
+				.license("License 2.0")
+				.licenseUrl("http://www.stacksimplify.com/license.html")
+				.build();
+	}
 	
 	
 	
---
+++ src/main/java/com/stacksimplify/restservices/services/Contact.java
@@ -0,0 +1,27 @@
+package com.stacksimplify.restservices.services;
+
+public class Contact {
+
+	private final String name;
+	private final String url;
+	private final String email;
+	public Contact(String name, String url, String email) {
+		
+		this.name = name;
+		this.url = url;
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public String getEmail() {
+		return email;
+	}
+	
+	
+	
+	
+}
