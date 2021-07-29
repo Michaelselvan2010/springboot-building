d99bd1fdf902e43f16b3077b50e52414da738086
Michael <michael.selvan2005@gmail.com>
2021-07-29 11:50:17 +0530

    ModelMapper - First Commit

---
+++ src/main/java/com/stacksimplify/restservices/controllers/UserModelMapperController.java
@@ -0,0 +1,51 @@
+package com.stacksimplify.restservices.controllers;
+
+import java.util.Optional;
+
+import javax.validation.constraints.Min;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stacksimplify.restservices.dtos.UserMmDto;
+import com.stacksimplify.restservices.entities.UserInfo;
+import com.stacksimplify.restservices.exceptions.UserNotFoundException;
+import com.stacksimplify.restservices.services.UserService;
+
+@RestController
+@RequestMapping("/modelmapper/users")
+public class UserModelMapperController {
+
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	
+	@GetMapping("/{id}")
+	public UserMmDto getUserById(@PathVariable("id") @Min(1) Long id) throws UserNotFoundException{
+		
+			Optional<UserInfo> userOptional= userService.getUserById(id);
+			if (!userOptional.isPresent()) {
+				throw new UserNotFoundException("User Not Found");
+			}
+			
+			
+			UserInfo userInfo=userOptional.get();
+			
+			UserMmDto userMmDto=modelMapper.map(userInfo, UserMmDto.class);
+			
+			return userMmDto;
+			
+			
+	}
+
+	
+	
+	
+}
